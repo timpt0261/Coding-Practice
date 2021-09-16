4bc0f13d2d57822d72cab28ed12277424068f559
Reuben Chavez <rchavez0215@gmail.com>
2021-09-16 16:26:36 -0700

    Calculator practice works well

---
+++ C++ Practice  Files/calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout<<"Enter the second number: ";
+    cin >> num2;
+
+    cout << "The answer is: " << num1 + num2;
+
+    return 0;
+}
